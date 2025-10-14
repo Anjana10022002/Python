4827b857226dcffc26c13d2083d41d9be1dbe6aa
Anjana P R <pranjana333@gmail.com>
2025-10-14 15:37:22 +0530

    Added a bonus volume using random module.

--- Python_Lab_Day1_Qn-1.py
+++ Python_Lab_Day1_Qn-1.py
@@ -1,12 +1,17 @@
+import random
+
 apple = 15.5
 orange = 20
 grape = 10.25
 
 totalVolume = apple + orange + grape
-print(totalVolume)
+print("Total Volume: ",totalVolume)
 
 totalVolume_int = int(totalVolume)
-print(totalVolume_int)
+print("Total Volume as integer: ",totalVolume_int)
 
-totalVolume_string = string(totalVolume)
-print(totalVolume_string)
+totalVolume_string = str(totalVolume)
+print("Total Volume as string: ",totalVolume_string)
+
+bonusVolume = totalVolume + random.randrange(5,10)
+print("Bonus volume: ",bonusVolume)
