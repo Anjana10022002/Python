6284493c03da5d4c1606ac5f978232b3b07f0ba5
Anjana P R <pranjana333@gmail.com>
2025-10-14 15:28:41 +0530

    Add initial scripts for fruit volume calculation and name printing

---
+++ Python_Lab_Day1_Qn-1.py
@@ -0,0 +1,12 @@
+apple = 15.5
+orange = 20
+grape = 10.25
+
+totalVolume = apple + orange + grape
+print(totalVolume)
+
+totalVolume_int = int(totalVolume)
+print(totalVolume_int)
+
+totalVolume_string = string(totalVolume)
+print(totalVolume_string)
---
+++ demo.py
@@ -0,0 +1,2 @@
+name = "Abin"
+print(name)
