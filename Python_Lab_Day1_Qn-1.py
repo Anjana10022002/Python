279380590e12f79805e5028a5ad40690e535c0c7
Anjana P R <pranjana333@gmail.com>
2025-10-14 16:33:36 +0530

    fixed syntax errors

--- Python_Lab_Day1_Qn-1.py
+++ Python_Lab_Day1_Qn-1.py
@@ -8,10 +8,10 @@ totalVolume = apple + orange + grape
 print("Total Volume: ",totalVolume)
 
 totalVolume_int = int(totalVolume)
-print("Total Volume as integer: ",totalVolume_int)
+print("Total Volume (integer): ",totalVolume_int)
 
 totalVolume_string = str(totalVolume)
-print("Total Volume as string: ",totalVolume_string)
+print("Total Volume (string): ",totalVolume_string)
 
 bonusVolume = totalVolume + random.randrange(5,10)
-print("Bonus volume: ",bonusVolume)
+print("Final volume with bonus: ",bonusVolume)
