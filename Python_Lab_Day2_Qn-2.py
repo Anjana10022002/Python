772a8713de2d5ade35240905e59ac2a0a4f31a8b
Anjana P R <pranjana333@gmail.com>
2025-10-15 16:58:15 +0530

    printing first and last character of the multi line string

--- Python_Lab_Day2_Qn-2.py
+++ Python_Lab_Day2_Qn-2.py
@@ -1,5 +1,7 @@
 python = """Python is a popular, high-level, and 
 versatile programming language known for its 
-simple syntax, making it easy to learn and read. """
+simple syntax, making it easy to learn and read."""
 print(python)
 print("The length of python: ",len(python))
+print("First character: ",python[0])
+print("Last character: ",python[-1])
