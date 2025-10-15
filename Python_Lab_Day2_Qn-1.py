77bb433992b79d02d3b007963748007a0f96a5de
Anjana P R <pranjana333@gmail.com>
2025-10-15 16:50:04 +0530

    Store a short paragraph about a Python course using a multiline string.

---
+++ Python_Lab_Day2_Qn-1.py
@@ -0,0 +1,6 @@
+receiptHeader = """XYZ Store
+Receipt"""
+items = '''Book Title: "Python Basics" – ₹450
+Book Title: "Data Science Intro" – ₹600'''
+print(receiptHeader)
+print(items)
---
+++ Python_Lab_Day2_Qn-2.py
@@ -0,0 +1,3 @@
+python = """Python is a popular, high-level, and versatile programming language known for its simple syntax,
+ making it easy to learn and read. It is used for a wide range of applications, including web development, 
+ data science, artificial intelligence, automation, and software development."""
