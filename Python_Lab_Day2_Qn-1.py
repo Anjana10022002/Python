9856def6f40876a81743149f1cf3991490bdd8e3
Anjana P R <pranjana333@gmail.com>
2025-10-15 22:37:07 +0530

    Display the entire receipt in uppercase

--- Python_Lab_Day2_Qn-1.py
+++ Python_Lab_Day2_Qn-1.py
@@ -1,10 +1,12 @@
-receiptHeader = """XYZ Store
-Receipt"""
-print(receiptHeader)
+receiptHeader = """XYZ Store \n
+Receipt \n"""
+print(receiptHeader.upper())
 item1 = "Book Title: {a}\t - {b}".format(a = 'Python Basics', b = '₹450')
-print(item1)
+print(item1.upper())
 item2 = "Book Title: {a}\t - {b}".format(a = 'Data Science Intro', b = '₹600')
-print(item2)
+print(item2.upper())
 Total = 450 + 600
 totalAmount = "Total amount:\t\t\t - {}".format(Total)
-print(totalAmount)
+print(totalAmount.upper())
+Thankyou = "\n\nThank you for shopping with us! \n"
+print(Thankyou.upper())
