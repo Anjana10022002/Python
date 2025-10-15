4e3c5da2e78e3bdb5ccaff560bf3aae713e22db1
Anjana P R <pranjana333@gmail.com>
2025-10-15 16:52:03 +0530

    Display the length of the paragraph (number of characters).

--- Python_Lab_Day2_Qn-2.py
+++ Python_Lab_Day2_Qn-2.py
@@ -1,3 +1,5 @@
-python = """Python is a popular, high-level, and versatile programming language known for its simple syntax,
- making it easy to learn and read. It is used for a wide range of applications, including web development, 
- data science, artificial intelligence, automation, and software development."""
+python = """Python is a popular, high-level, and 
+versatile programming language known for its 
+simple syntax, making it easy to learn and read. """
+print(python)
+print("The length of python: ",len(python))
