76ee39eefe91d2dfab1d88e1c36407c49479caa4
Anjana P R <pranjana333@gmail.com>
2025-10-16 20:29:46 +0530

    Check if user_credits is within the valid range (greater than or equal to min_credits and less than or equal to max_credits) and not equal to min_credits. Store the result in credits_valid.

--- Python_Lab_Day3_Qn-2.py
+++ Python_Lab_Day3_Qn-2.py
@@ -3,3 +3,4 @@ is_subscribed = False
 user_credits = 100
 max_credits = 200
 min_credits = 50
+credits_valid = (user_credits >= min_credits and user_credits <= max_credits) and (user_credits != min_credits)
