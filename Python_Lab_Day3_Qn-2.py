d423a1a090f5b6b15b89e70ba5aa00895d6b9a0d
Anjana P R <pranjana333@gmail.com>
2025-10-16 20:30:45 +0530

    Check if the user is eligible for bonus credits: the user is eligible if they are either subscribed or have credits greater than min_credits. Use the or and not operators. Store the result in bonus_eligible.

--- Python_Lab_Day3_Qn-2.py
+++ Python_Lab_Day3_Qn-2.py
@@ -4,3 +4,4 @@ user_credits = 100
 max_credits = 200
 min_credits = 50
 credits_valid = (user_credits >= min_credits and user_credits <= max_credits) and (user_credits != min_credits)
+bonus_eligible = is_subscribed or not (user_credits <= min_credits)
