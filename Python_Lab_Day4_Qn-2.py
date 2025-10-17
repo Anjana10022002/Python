9b185944375f0f6b2c55088b7a0b37f33186222b
Anjana P R <pranjana333@gmail.com>
2025-10-17 15:22:38 +0530

    Created a list for each workshop containing the names of 3 participants.

---
+++ Python_Lab_Day4_Qn-2.py
@@ -0,0 +1,3 @@
+Web_Development = ["Participant1", "Participant2", "Participant3"]
+Data_Science = ["Participant1", "Participant2", "Participant3"]
+UIUX_Design = ["Participant1", "Participant2", "Participant3"]
