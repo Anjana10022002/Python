ef9f3a9c2f229b7bb986f40fc46db201fba79c34
Anjana P R <pranjana333@gmail.com>
2025-10-17 15:55:02 +0530

    Copy the list of Data Science participants to a new list and clear the original.

--- Python_Lab_Day4_Qn-2.py
+++ Python_Lab_Day4_Qn-2.py
@@ -4,4 +4,6 @@ UIUX_Design = ["David", "Jane", "Jose"]
 all_participants = Web_Development + Data_Science + UIUX_Design
 Web_Development.append("Harry")
 Data_Science.insert(1, "Louis")
-print(Data_Science)
+UIUX_Design.pop()
+new_Datascience = Data_Science.copy()
+del Data_Science
