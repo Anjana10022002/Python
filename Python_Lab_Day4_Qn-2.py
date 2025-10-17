844286421760844f147aae44aae4a01e776864be
Anjana P R <pranjana333@gmail.com>
2025-10-17 15:26:48 +0530

    Combine all three workshop lists into a single nested list called all_participants.

--- Python_Lab_Day4_Qn-2.py
+++ Python_Lab_Day4_Qn-2.py
@@ -1,3 +1,5 @@
 Web_Development = ["Participant1", "Participant2", "Participant3"]
 Data_Science = ["Participant1", "Participant2", "Participant3"]
 UIUX_Design = ["Participant1", "Participant2", "Participant3"]
+all_participants = Web_Development + Data_Science + UIUX_Design
+print(all_participants)
