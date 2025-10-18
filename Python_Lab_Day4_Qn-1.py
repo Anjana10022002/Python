a4f31574c169bc7f2d4e46b8a8bd57a60ae9181a
Anjana P R <pranjana333@gmail.com>
2025-10-18 22:12:40 +0530

    Insert a new item at the second position of the vegetables list.

--- Python_Lab_Day4_Qn-1.py
+++ Python_Lab_Day4_Qn-1.py
@@ -2,3 +2,4 @@ fruits = ["apple", "orange", "grape"]
 vegetables = ["carrot", "cabbage", "spinach"]
 beverages = ["water", "juice", "coffee"]
 fruits.append("dragonfruit")
+vegetables.insert(2, "broccoli")
