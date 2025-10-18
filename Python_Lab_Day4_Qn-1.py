7333c8d4041f963580af59c509000f512d217e24
Anjana P R <pranjana333@gmail.com>
2025-10-18 22:20:12 +0530

    Use negative indexing to access the last item from the vegetables list.

--- Python_Lab_Day4_Qn-1.py
+++ Python_Lab_Day4_Qn-1.py
@@ -6,3 +6,4 @@ vegetables.insert(2, "broccoli")
 beverages.pop()
 inventory = fruits + vegetables + beverages
 print("First two fruits:", fruits[:2])
+print("First two vegetables:", vegetables[:-2])
