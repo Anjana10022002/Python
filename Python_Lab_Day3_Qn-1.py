7bde48d1af5460eb733c5369d7870561490366e2
Anjana P R <pranjana333@gmail.com>
2025-10-19 22:57:01 +0530

    The platform allows login only if the user’s age is greater than or equal to 18. Create a variable user_age and assign it 17. Use a comparison operator to check if the age is valid. Store the result in is_age_valid.

--- Python_Lab_Day3_Qn-1.py
+++ Python_Lab_Day3_Qn-1.py
@@ -2,5 +2,6 @@ has_account = True
 email_verified = False
 can_login = has_account is True and email_verified is True
 email = abcdef@gmail.com
-email_valid = "@" in email
-
+is_email_valid = "@" in email
+user_age = 17
+is_age_valid = user_age >= 18
