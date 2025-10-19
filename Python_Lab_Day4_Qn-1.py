2910918c848b02a2608ef0d3f072cb757f9259ae
Anjana P R <pranjana333@gmail.com>
2025-10-19 22:33:26 +0530

    Check if "Water" is in the beverages list.

--- Python_Lab_Day4_Qn-1.py
+++ Python_Lab_Day4_Qn-1.py
@@ -9,3 +9,7 @@ print("First two fruits:", fruits[:2])
 print("First two vegetables:", vegetables[:-2])
 name_lengths = [len(name) for name in fruits]
 print("Length of each name in Fruits list:", name_lengths)
+if "water" in beverages:
+    print("Yes, 'water' is in the beverage list")
+else:
+    print("No, 'water' is not in the beverage list")
