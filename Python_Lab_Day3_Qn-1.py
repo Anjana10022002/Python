b3cc8df69affba1d64f6585a21574335f0aff14c
Anjana P R <pranjana333@gmail.com>
2025-10-19 22:55:32 +0530

    To validate an email address, check if it contains the @ symbol, which is required for a valid email. For example, if the email is "g@example.com", confirm that @ is present. Store the result in a Boolean variable is_email_valid.

--- Python_Lab_Day3_Qn-1.py
+++ Python_Lab_Day3_Qn-1.py
@@ -1,4 +1,6 @@
 has_account = True
 email_verified = False
-can_login = has_account is True and email_verified is true:
-    print()
+can_login = has_account is True and email_verified is True
+email = abcdef@gmail.com
+email_valid = "@" in email
+
