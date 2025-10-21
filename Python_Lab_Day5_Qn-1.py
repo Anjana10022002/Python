9cf544198ee2d1dcd15ce449a5e964762cabb3fd
Anjana P R <pranjana333@gmail.com>
2025-10-21 15:44:40 +0530

    Add a new student to the Python set. Remove one student from the Data Science set. Find and display the names of students who are enrolled in both courses.

--- Python_Lab_Day5_Qn-1.py
+++ Python_Lab_Day5_Qn-1.py
@@ -2,5 +2,6 @@ Python = {"Ajay", "Max", "John", "Sara"}
 DataScience = {"John", "Mike", "Sara", "Linda"}
 Python.add("Alana")
 DataScience.pop()
-bothCourse = 
+bothCourse = Python.intersectiona(DataScience)
+print("Students enrolled in both the courses: ", bothCourse)
  
