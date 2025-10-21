23cb564dbc68858f29e2152743cd4de32b1ccf28
Anjana P R <pranjana333@gmail.com>
2025-10-21 21:59:14 +0530

    Find students who are only in the Python course but not in Data Science.

--- Python_Lab_Day5_Qn-1.py
+++ Python_Lab_Day5_Qn-1.py
@@ -2,6 +2,7 @@ Python = {"Ajay", "Max", "John", "Sara"}
 DataScience = {"John", "Mike", "Sara", "Linda"}
 Python.add("Alana")
 DataScience.pop()
-bothCourse = Python.intersectiona(DataScience)
+bothCourse = Python.intersection(DataScience)
 print("Students enrolled in both the courses: ", bothCourse)
-onlyPython = Python - 
+onlyPython = Python - DataScience
+print("Students enrolled only in Python: ", onlyPython)
