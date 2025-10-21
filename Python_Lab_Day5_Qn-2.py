66fe39452bc2cd2967c6db3996df9418e2b2cd23
Anjana P R <pranjana333@gmail.com>
2025-10-21 15:28:34 +0530

    Using dictionary comprehension, create a new dictionary that adds a "Fullstack" course by combining student counts from both existing courses.

--- Python_Lab_Day5_Qn-2.py
+++ Python_Lab_Day5_Qn-2.py
@@ -7,3 +7,14 @@ print("Students enrolled in both the courses: ", bothCourses)
 onlyBackend = backend - frontend
 print("Students enrolled only in backend: ",onlyBackend)
 print("Total number of unique students: ", len(bothCourses))
+
+course = {
+    "Frontend" : len(frontend),
+    "Backend" : len(backend)
+    }
+for courseName, student in course.items():
+    print("The couse name and number of students in courses are:", courseName, student)
+dictComprehension = {
+    "Fullstack" : len(frontend) + len(backend)
+}
+print("Fullstack couses: ", dictComprehension)
