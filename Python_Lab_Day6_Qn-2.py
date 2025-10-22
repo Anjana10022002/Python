cc655c9902079f3c6b09c5eed0fcc8b1b1603ab4
Anjana P R <pranjana333@gmail.com>
2025-10-22 15:42:17 +0530

    Print how many posts were "Trending"

--- Python_Lab_Day6_Qn-2.py
+++ Python_Lab_Day6_Qn-2.py
@@ -2,11 +2,14 @@ blog_views = [150, 800, 2500, 600, 1200, 450, 3000]
 for x in blog_views:
     if x > 1000:
         print("Trending")
-    elif x >= 500 and x >= 1000:
+    elif 500 <= x <= 1000:
         print("Average")
-    elif x <500:
+    elif x < 500:
         print("Low Traffic")
 Total_views = sum(blog_views)
 print("Total number of views:", Total_views)
-count = blog_views.count("Trending")
-print("Number of trending posts: ", count)
+count = 0
+for x in blog_views:
+    if x > 1000:
+        count = count + 1
+print("Count of trending posts: ", count)
