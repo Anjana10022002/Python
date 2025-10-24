baad6b1fcc409f8b1d9a6f7c61ec78dae3359e24
Anjana P R <pranjana333@gmail.com>
2025-10-24 22:34:39 +0530

    Added new element to the list

--- Python_Lab_Day7_Qn-1.py
+++ Python_Lab_Day7_Qn-1.py
@@ -1,5 +1,9 @@
 initial_grocery = ["milk", "bread", "eggs"]
-def add_item(grocery_list, item):
-    grocery_list.append(item)
-    print(grocery_list)
-print
+def add_item(item):
+    initial_grocery.append(item)
+    print("Updated grocery list: ",initial_grocery)
+def remove_last_Item():
+    removeItem = initial_grocery.pop()
+    print("Updated grocery list after removing last item: ", initial_grocery)
+add_item("butter")
+remove_last_Item()
