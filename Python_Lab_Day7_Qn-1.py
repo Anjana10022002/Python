9fb7898054fe513db85c1a88388aeacabde92188
Anjana P R <pranjana333@gmail.com>
2025-10-25 13:11:59 +0530

    Write a recursive function count_characters(items) that returns the total number of characters in all item names combined. For example, ["milk", "bread"] has 4 + 5 = 9 characters..

--- Python_Lab_Day7_Qn-1.py
+++ Python_Lab_Day7_Qn-1.py
@@ -1,10 +1,13 @@
 initial_grocery = ["milk", "bread", "eggs"]
 def add_item(item):
     initial_grocery.append(item)
-    print("Updated grocery list: ",initial_grocery)
 def remove_last_Item():
     removeItem = initial_grocery.pop()
-    print("Updated grocery list after removing last item: ", initial_grocery)
-add_item("butter")
-remove_last_Item()
 show_item = lambda item: print(f"Item: {item}")
+for i in initial_grocery:
+    show_item(i)
+def count_characters(x):
+    if not x:
+        return 0
+    else:
+        return len(item[0]) + count_characters(item[1:])
