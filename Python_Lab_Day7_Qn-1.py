762363d7d37d2603a42fcaf3055cb8c257d4ecfb
Anjana P R <pranjana333@gmail.com>
2025-10-25 13:26:00 +0530

    Refactor grocery list functions and enhance output formatting

--- Python_Lab_Day7_Qn-1.py
+++ Python_Lab_Day7_Qn-1.py
@@ -1,13 +1,32 @@
 initial_grocery = ["milk", "bread", "eggs"]
+
 def add_item(item):
     initial_grocery.append(item)
+
 def remove_last_Item():
     removeItem = initial_grocery.pop()
+
 show_item = lambda item: print(f"Item: {item}")
-for i in initial_grocery:
-    show_item(i)
+
 def count_characters(x):
     if not x:
         return 0
     else:
-        return len(item[0]) + count_characters(item[1:])
+        return len(x[0]) + count_characters(x[1:])
+    
+print("Initial grocery list: ")
+for x in initial_grocery:
+    show_item(x)
+
+add_item("sugar")
+print("After adding new item")
+for x in initial_grocery:
+    show_item(x)
+
+remove_last_Item()
+print("Gracery list after removing last item: ")
+for x in initial_grocery:
+    show_item(x)
+
+total_characters = count_characters(initial_grocery)
+print("Total number of characters:", total_characters)
