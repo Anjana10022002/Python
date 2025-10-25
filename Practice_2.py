75e50435cea3b6faf65af6a06f1a2e1f1f6c7e93
Anjana P R <pranjana333@gmail.com>
2025-10-25 23:11:31 +0530

    a function add_student(name) to add a new student to the list.

---
+++ Practice_2.py
@@ -0,0 +1,20 @@
+"""
+Question: Student Name Tracker
+
+Create a Python program that:
+
+Starts with a list of student names: ["Alice", "Bob", "Charlie"].
+
+Has a function add_student(name) to add a new student to the list.
+
+Has a function remove_last_student() to remove the last student from the list.
+
+Uses a lambda function to print each student in the format: "Student: <name>".
+
+Includes a recursive function count_total_letters(names) that returns the total number of letters in all student names combined.
+"""
+student = ["Alice", "Bob", "Charlie"]
+
+def add_name():
+    student.append()
+    
