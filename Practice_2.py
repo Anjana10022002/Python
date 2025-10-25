178316d494ffd5f0d7829e4fa12395fbba11cd9c
Anjana P R <pranjana333@gmail.com>
2025-10-25 23:20:50 +0530

    calling all the functions and displaying.

--- Practice_2.py
+++ Practice_2.py
@@ -15,12 +15,32 @@ Includes a recursive function count_total_letters(names) that returns the total 
 """
 student = ["Alice", "Bob", "Charlie"]
 
-def add_student():
-    student.append()
+def add_student(x):
+    student.append(x)
 
 def remove_last_student():
-    student,pop()
+    student.pop()
 
 show_student= lambda x: print(f"Student:{x}")
 
+def count_total_letters(x):
+    if not x:
+        return 0 
+    else:
+        return len(x[0]) - count_total_letters(x[1:])
 
+print("Student list:")
+for x in student:
+    show_student(x)
+
+add_student("Alex")
+print("Student list sfter adding: ")
+for x in student:
+    show_student(x)
+
+remove_last_student()
+print("Student list after removing last student name: ")
+for x in student:
+    show_student(x)
+
+print
