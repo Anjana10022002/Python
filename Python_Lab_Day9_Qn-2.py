4e45a0291aa5871679770d3798e9842c7dda1dc1
Anjana P R <pranjana333@gmail.com>
2025-10-27 22:47:01 +0530

    fixing syntax error

--- Python_Lab_Day9_Qn-2.py
+++ Python_Lab_Day9_Qn-2.py
@@ -1,9 +1,9 @@
 class Account:
-    def __init__ (self, hName, balance):
+    def __init__(self, hName, balance):
         self._holderName = hName
         self._balance = balance
 
-    def __add__ (self, other):
+    def __add__(self, other):
         return self._balance + other._balance
 
 class SavingsAccount(Account):
@@ -14,8 +14,10 @@ class CurrentAccount(Account):
     def calculate_interest(self):
         return self._balance * 0.02
     
-savings_account1 = Account("Ravi", 10000)
-savings_account2 = Account("Anjali", 15000)
+savings_account = SavingsAccount("Ravi", 10000)
+current_account = CurrentAccount("Anjali", 15000)
 
-print()
-    
+print("Savings account: ")
+print(f"Name: {savings_account._holderName}")
+print(f"Balance: {savings_account._balance}")
+print(f"Interest: {savings_account.calculate_interest()}")
