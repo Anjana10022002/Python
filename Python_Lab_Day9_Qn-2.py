de31f209210d238cac0dd77817da3edbed251c3d
Anjana P R <pranjana333@gmail.com>
2025-10-27 15:56:43 +0530

    Create a class called CurrentAccount that inherits from Account and has a method calculate_interest that returns the interest as balance * 0.02 (2% interest).

--- Python_Lab_Day9_Qn-2.py
+++ Python_Lab_Day9_Qn-2.py
@@ -1,6 +1,11 @@
 class Account:
-    def __init__ (self):
-        self._holderName = "Ben"
-        self._balance = 100.0
-class SavingsAccount()
+    def __init__ (self, hName, balance):
+        self._holderName = hName
+        self._balance = balance
 
+    
+class SavingsAccount(Account):
+    def __init__(self, hName, balance):
+        super().__init__(hName, balance)
+        claculate_interest = balance * 0.02
+        return claculate_interest
