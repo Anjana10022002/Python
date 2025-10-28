49e30c641600d8929cc34f1c3d1e64dfe9c1adfa
Anjana P R <pranjana333@gmail.com>
2025-10-28 14:40:33 +0530

    Include a constructor to initialize _vehicle_id and _base_rate. Provide a method display_details() to return a string with the vehicle ID and base rate.

---
+++ Python_Lab_Day9_Qn-1.py
@@ -0,0 +1,6 @@
+class vehicle:
+    def __init__(self, vehicle_id, base_rate):
+        self._vehicle_id = vehicle_id
+        self._base_rate = base_rate
+    def display_details():
+        return f"Vehicle id: {self.vehicle_id}, base rate_ {self.baseRate}"
