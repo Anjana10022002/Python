02a4d84e0e4406c15287ee455f8d52aceb344c31
Anjana P R <pranjana333@gmail.com>
2025-10-28 23:46:05 +0530

    error fixing

--- Python_Lab_Day9_Qn-1.py
+++ Python_Lab_Day9_Qn-1.py
@@ -8,23 +8,24 @@ class Vehicle:
         return 0.0
     
 class Car(Vehicle):
-    def __init__(self, vehicle_id, base_rate, num_rate):
-        super().__init__(vehicle_id, base_rate, num_rate)
-        self._num_rate = num_rate
+    def __init__(self, vehicle_id, base_rate, num_seats):
+        super().__init__(vehicle_id, base_rate)
+        self._num_seats = num_seats
 
-    def rental_charges(self, num_seats, base_rate):
-        daily_rental = self.base_rate + self.num_seats
+    def rental_charges(self):
+        daily_rental = self._base_rate + self._num_seats
         return daily_rental
         
 class Bike(Vehicle):
     def __init__(self, vehicle_id, base_rate, bike_type):
-        super().__init__(vehicle_id, base_rate, bike_type)
+        super().__init__(vehicle_id, base_rate)
+        self._bike_type = bike_type
         self.bike_type = bike_type
 
     def rental_charges(self, bike_type, base_rate):
         daily_rental = self._base_rate * 0.5
         return daily_rental
     
-    
+
 
 
