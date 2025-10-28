522d08a6ee9d33a28ed037004436d9dc666ac984
Anjana P R <pranjana333@gmail.com>
2025-10-28 14:45:43 +0530

    Create a subclass Car that inherits from Vehicle:      Add an attribute num_seats (integer, e.g., 4 for a sedan).      Override the rental_charge() method to calculate the daily rental charge as _base_rate * num_seats.

--- Python_Lab_Day9_Qn-1.py
+++ Python_Lab_Day9_Qn-1.py
@@ -4,3 +4,12 @@ class vehicle:
         self._base_rate = base_rate
     def display_details():
         return f"Vehicle id: {self.vehicle_id}, base rate_ {self.baseRate}"
+    def rental_charge():
+        return 0.0
+    
+    class car(vehicle):
+        def rental_charges(self, num_seats):
+            daily_rental = self.base_rate + self.num_seats
+            return daily_rental
+        
+    class
