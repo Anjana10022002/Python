52b2c05933c3ea724f79aab56d3f955dede63756
Anjana P R <pranjana333@gmail.com>
2025-10-28 23:44:37 +0530

    fixing syntax error

--- Python_Lab_Day9_Qn-1.py
+++ Python_Lab_Day9_Qn-1.py
@@ -1,4 +1,4 @@
-class vehicle:
+class Vehicle:
     def __init__(self, vehicle_id, base_rate):
         self._vehicle_id = vehicle_id
         self._base_rate = base_rate
@@ -7,9 +7,24 @@ class vehicle:
     def rental_charge():
         return 0.0
     
-    class car(vehicle):
-        def rental_charges(self, num_seats):
-            daily_rental = self.base_rate + self.num_seats
-            return daily_rental
+class Car(Vehicle):
+    def __init__(self, vehicle_id, base_rate, num_rate):
+        super().__init__(vehicle_id, base_rate, num_rate)
+        self._num_rate = num_rate
+
+    def rental_charges(self, num_seats, base_rate):
+        daily_rental = self.base_rate + self.num_seats
+        return daily_rental
         
-    class
+class Bike(Vehicle):
+    def __init__(self, vehicle_id, base_rate, bike_type):
+        super().__init__(vehicle_id, base_rate, bike_type)
+        self.bike_type = bike_type
+
+    def rental_charges(self, bike_type, base_rate):
+        daily_rental = self._base_rate * 0.5
+        return daily_rental
+    
+    
+
+
