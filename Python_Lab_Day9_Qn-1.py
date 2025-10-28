61fc26ca7e0a2fafae2cd851991fe5181b67a580
Anjana P R <pranjana333@gmail.com>
2025-10-28 23:47:27 +0530

    create a new function to dispaly the  details.

--- Python_Lab_Day9_Qn-1.py
+++ Python_Lab_Day9_Qn-1.py
@@ -15,7 +15,10 @@ class Car(Vehicle):
     def rental_charges(self):
         daily_rental = self._base_rate + self._num_seats
         return daily_rental
-        
+
+    def display_details(self):
+        return f"Vehicle id: {self._vehicle_id}, base rate: {self._base_rate}, number of seats: {self._num_seats}"
+
 class Bike(Vehicle):
     def __init__(self, vehicle_id, base_rate, bike_type):
         super().__init__(vehicle_id, base_rate)
