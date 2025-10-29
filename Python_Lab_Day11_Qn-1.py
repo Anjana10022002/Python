5b28a68eddc603b79e38a28255fa3b9107b1819b
Anjana P R <pranjana333@gmail.com>
2025-10-29 23:36:31 +0530

    fixing syntax error

--- Python_Lab_Day11_Qn-1.py
+++ Python_Lab_Day11_Qn-1.py
@@ -1,9 +1,13 @@
-from tripdata import TripData
+from tripdata import trip_details
+from datetime import datetime
 import json
 
 trips = [
-    TripData("India", "10-02-2025", "discovering the beauty of India"),
-    TripData("Italy", "15-03-2025", "exploring the historic sites of Italy"),
-    TripData("Japan", "20-04-2025", "experiencing the culture of Japan")
+    trip_details("India", "10-02-2025", "discovering the beauty of India"),
+    trip_details("Italy", "15-03-2025", "exploring the historic sites of Italy"),
+    trip_details("Japan", "20-04-2025", "experiencing the culture of Japan")
 ]
 
+for x in trips:
+    dateObject = datetime.strptime(x["date"], "%d-%m-%y")
+    x["date"] = dateObject.strftime("%B %d %y")
