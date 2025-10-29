918305f1ccdb569331a8977d4a4533371b15cbc1
Anjana P R <pranjana333@gmail.com>
2025-10-29 23:29:42 +0530

    In your main file, import the function and use it to build a list of trip dictionaries.

--- Python_Lab_Day11_Qn-1.py
+++ Python_Lab_Day11_Qn-1.py
@@ -1,3 +1,9 @@
-import tripdata as td
+from tripdata import TripData
 import json
+
+trips = [
+    TripData("India", "10-02-2025", "discovering the beauty of India"),
+    TripData("Italy", "15-03-2025", "exploring the historic sites of Italy"),
+    TripData("Japan", "20-04-2025", "experiencing the culture of Japan")
+]
 
