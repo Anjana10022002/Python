795e3d298175a06c25d6a96146c9b2cd01e9eb32
Anjana P R <pranjana333@gmail.com>
2025-10-30 15:54:54 +0530

    If the book title contains only alphabets and spaces, accept it. Otherwise, show an error.

--- Pyhton_Lab_Day12_Qn-1.py
+++ Pyhton_Lab_Day12_Qn-1.py
@@ -1,4 +1,15 @@
 import re
+import sys
 
 title = input("Enter the book title: ")
 year = input("Enter the publication year: ")
+
+pattern = '^[a-zA-Z ]+$'
+result = re.search(pattern, title)
+if result:
+    print("Accepted!")
+else:
+    print("Oops!", sys.exc_info()[0], "occurred.")
+
+pattern = '^.19|20'
+result = re.search(pattern, year)
