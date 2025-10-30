5b4fca1d12a563c3bcb010faf8f9a24ebbc62e73
Anjana P R <pranjana333@gmail.com>
2025-10-30 12:27:59 +0530

    Convert the date string of each record into a readable format like "Month Day, Year" (e.g., "June 05, 2022").

--- Python_Lab_Day11_Qn-2.py
+++ Python_Lab_Day11_Qn-2.py
@@ -2,9 +2,13 @@ from tracker import travel_data
 import datetime
 import json
 
-trips = [
+records = [
     travel_data("India", "Delhi", "discovering the beauty of India"),
     travel_data("Italy", "Rome", "exploring the historic sites of Italy"),
     travel_data("Japan", "Tokyo", "experiencing the culture of Japan")
 ]
+
+for x in records:
+    date_obj = datetime.strptime(x["date"], "%d-%m-%y")
+    x["date"] = date_obj.strftime("%B %d, %Y")
 
