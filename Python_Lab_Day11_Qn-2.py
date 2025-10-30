cf5cdc398620929fab25fbc4a812b47fe340d479
Anjana P R <pranjana333@gmail.com>
2025-10-30 12:24:23 +0530

    Create a module called tracker.py that contains a function to return a travel record (as a dictionary). In the main file, use the function to create a list of at least 3 travel records.

---
+++ Python_Lab_Day11_Qn-2.py
@@ -0,0 +1,10 @@
+from tracker import travel_data
+import datetime
+import json
+
+trips = [
+    travel_data("India", "Delhi", "discovering the beauty of India"),
+    travel_data("Italy", "Rome", "exploring the historic sites of Italy"),
+    travel_data("Japan", "Tokyo", "experiencing the culture of Japan")
+]
+
---
+++ tracker.py
@@ -0,0 +1,5 @@
+def travel_data(place, city, comment):
+    data = {"place" : place,
+            "city" : city,
+            "comments" : comment}
+    return data 
