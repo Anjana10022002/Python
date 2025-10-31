b01f4e1cc12da61a182d25aedb2d2968ad1f62bb
Anjana P R <pranjana333@gmail.com>
2025-10-31 15:35:37 +0530

    file exist condition

--- Python_Lab_Day13_Qn-1.py
+++ Python_Lab_Day13_Qn-1.py
@@ -4,6 +4,8 @@ item = input("Enter the name of new item: ")
 if os.path.exists("items.txt"):
     with open("items.txt", "a") as f:
         f.write(item )
-
-
+else:
+    with open("items.txt", "w") as f:
+        f.write(item )
+        
 
