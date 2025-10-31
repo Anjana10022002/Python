9b8d18365faf3cf3ced27b6948ca6c17f2892462
Anjana P R <pranjana333@gmail.com>
2025-10-31 23:46:41 +0530

    fixing syntax error

--- Python_Lab_Day13_Qn-1.py
+++ Python_Lab_Day13_Qn-1.py
@@ -3,10 +3,10 @@ import os
 item = input("Enter the name of new item: ")
 if os.path.exists("items.txt"):
     with open("items.txt", "a") as f:
-        f.write(item +"\n")
+        f.write(item + "\n")
 else:
     with open("items.txt", "w") as f:
-        f.write(item +"\n" )
+        f.write(item + "\n" )
 
 print("THe list is:") 
 with open("items.txt", "r") as f:
