cf9cce92175bd1d48ad88e97e3929c2ea6d5b181
Anjana P R <pranjana333@gmail.com>
2025-11-02 23:30:04 +0530

    checking for file and printing contaents in the file.

--- Python_Lab_Day13_Qn-2.py
+++ Python_Lab_Day13_Qn-2.py
@@ -1,4 +1,8 @@
 import os
 count = int(input("Enter the number of students wanted to add: ") )
 if os.path.exists("students.txt"): 
-    
+    with open("students.txt", "a") as f:
+        existing_names = f.readlines()
+        if existing_names:
+            for x in existing_names:
+                print(x.strip())
