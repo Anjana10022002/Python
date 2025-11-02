5b45904035266e59c3b09960fbd476ae6589c639
Anjana P R <pranjana333@gmail.com>
2025-11-02 23:25:27 +0530

    Taking input from the user

--- Python_Lab_Day13_Qn-2.py
+++ Python_Lab_Day13_Qn-2.py
@@ -1,3 +1,4 @@
 import os
-count = int(input("Enter the number of students to add: ") )
+count = int(input("Enter the number of students wanted to add: ") )
 if os.path.exists("students.txt"): 
+    
