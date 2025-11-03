80206781b4ce225bdc947cc6e5e25943580a6276
Anjana P R <pranjana333@gmail.com>
2025-11-03 22:21:45 +0530

    Fixing syntax error

--- Python_Lab_Day14_Qn-2.py
+++ Python_Lab_Day14_Qn-2.py
@@ -7,17 +7,14 @@ participant_list = name.split(",")
 new_list = list(set(participant_list))
 print("Customers name after removing duplication: ", new_list)
 
-shuffled_list = random.shuffle(new_list)
-print("Shuffled final list of names: ", shuffled_list)
+random.shuffle(new_list)
+print("Shuffled final list of names: ", new_list)
 
 random_customer = random.sample(new_list, 2)
 print("Randomly selected 2 customers: ", random_customer)
 
-def reverse(s):
-    s = s[::-1]
-    return s
-s = random_customer
-print("The reversed string is: ", reverse(s))
+for x in random_customer:
+    print("The reversed string is: ", x[::-1])
 
 count = len(new_list)
 print("The total number of unique participants: ", count)
