2b1cf0d6caf01b5c7194f6d3ab9c660d5998005b
Anjana P R <pranjana333@gmail.com>
2025-11-03 15:33:29 +0530

    Ask the user to enter a list of names of invited guests (comma-separated).

---
+++ Python_Lab_Day14_Qn-1.py
@@ -0,0 +1,9 @@
+invited_guests = input("Enter the list of names of invited guests (comma-separated): ")
+list = invited_guests.split(",")
+
+remove_duplication = []
+for x in list:
+    if x not in remove_duplication:
+        remove_duplication.append(x)
+
+print("After removing duplicate names:" + str(remove_duplication))
