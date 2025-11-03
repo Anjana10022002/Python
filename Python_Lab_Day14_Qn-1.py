5e520c81dbc90b87ba85b294bbeb5bef695fb425
Anjana P R <pranjana333@gmail.com>
2025-11-03 15:54:18 +0530

    Also, print the total number of unique names and round the square root of that number to the nearest whole number.

--- Python_Lab_Day14_Qn-1.py
+++ Python_Lab_Day14_Qn-1.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 invited_guests = input("Enter the list of names of invited guests (comma-separated): ")
 list = invited_guests.split(",")
@@ -12,4 +13,18 @@ print("After removing duplicate names:" + str(remove_duplication))
 
 random = random.choice(remove_duplication)
 print("Randomly chooosed guest: ", random)
+
+def reverse(s):
+    if len(s) == 0:
+        return s
+    else:
+        return reverse(s[1:]) + s[0]
+s = remove_duplication[1]
+print("The reversed string is: ", reverse(s))
+
+count = len(remove_duplication)
+print("Total number of unique names: ", count)
+print("Rounded square root of total unique names: ", math.ceil(math.sqrt(count)))
+
+
 
