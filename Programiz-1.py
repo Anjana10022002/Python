d0489eedc64e7dd10be587dfb14a038c99777d34
Anjana P R <pranjana333@gmail.com>
2025-11-04 15:48:17 +0530

    Python Program to Check Prime Number

--- Programiz-1.py
+++ Programiz-1.py
@@ -97,3 +97,17 @@
 # else:
 #     print(f"{c} is the largest number")
 
+# Qn-14
+num = int(input("Enter the number: "))
+if num == 0 or num == 1:
+    print(f"{num} is not a prime number.")
+elif num > 1:
+    for i in range(2, num):
+        if(num % i) == 0:
+            print(f"{num} is a prime number.")
+            break
+        else:
+            print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
+
