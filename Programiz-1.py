125a70a2e5b59c175098fd578503caee6f39d4db
Anjana P R <pranjana333@gmail.com>
2025-11-04 15:07:07 +0530

    Printing random number

--- Programiz-1.py
+++ Programiz-1.py
@@ -23,11 +23,31 @@
 # print("The area of triangle is: " ,area)
 
 # Qn-5
-import cmath
-a = int(input("Enter coefficient a: "))
-b = int(input("Enter coefficient b: "))
-c = int(input("Enter coefficient c: "))
-d = (b**2) - (4*a*c)
-solution1 = (-b + cmath.sqrt(d)) / (2*a)
-solution2 = (-b - cmath.sqrt(d)) / (2*a)
-print(f"THe solution of quadratic equation are {solution1} and {solution2}")
+# import cmath
+# a = int(input("Enter coefficient a: "))
+# b = int(input("Enter coefficient b: "))
+# c = int(input("Enter coefficient c: "))
+# d = (b**2) - (4*a*c)
+# solution1 = (-b + cmath.sqrt(d)) / (2*a)
+# solution2 = (-b - cmath.sqrt(d)) / (2*a)
+# print(f"THe solution of quadratic equation are {solution1} and {solution2}")
+
+# Qn-6
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# print(f"The number before swapping are {a} and {b}")
+# # a = a + b
+# # b = a - b
+# # a = a - b
+# temp = a
+# a = b
+# b = temp
+# print(f"The numbers after swapping are {a} and {b}")
+
+# Qn-7
+# import random
+# # random_num = random.random()
+# random_num = random.randint(10,100)
+# print("The random number is: ", random_num)
+
+# 
