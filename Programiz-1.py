7402e2633d66ae894a13c085685a646f713aa620
Anjana P R <pranjana333@gmail.com>
2025-11-04 14:55:56 +0530

    Initial commit of the program with various mathematical functions and input handling.

---
+++ Programiz-1.py
@@ -0,0 +1,32 @@
+# Qn-1
+# print("Hello World")
+
+# Qn-2
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# sum = a + b
+# print("The sum is: ", sum)
+
+# Qn-3
+# import math
+# a = int(input("Enter the number: "))
+# square = a ** 0.5
+# # square = math.sqrt(a) 
+# print("The square root of the number is: ", square)
+
+# Qn-4
+# a = int(input("Enter the first side:"))
+# b = int(input("ENter the second number: "))
+# c = int(input("Enter the third number: "))
+# s = (a+b+c)/2
+# area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+# print("The area of triangle is: " ,area)
+
+# Qn-5
+import cmath
+a = int(input("Enter coefficient a: "))
+b = int(input("Enter coefficient b: "))
+c = int(input("Enter coefficient c: "))
+d = (b**2) - (4*a*c)
+solution1 = (-b + cmath.sqrt(d)) / (2*a)
+solution2 = (-b - cmath.sqrt(d)) / (2*a)
