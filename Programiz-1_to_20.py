d88cb752da16c8c4f1a66af33c25e9eef58d36ca
Anjana P R <pranjana333@gmail.com>
2025-11-05 14:57:17 +0530

    Printing armstrong number in a specified interval .

--- Programiz-1_to_20.py
+++ Programiz-1_to_20.py
@@ -122,4 +122,29 @@
 #         else:
 #             print(x)
 
-# Qn=16
+# Qn-16
+
+# Qn-17
+
+# Qn-18
+
+# Qn-19
+
+# Qn-20
+# num = int(input("Enter the number: "))
+# order = len(str(num))
+# sum = 0
+# temp = num
+# while temp > 0:
+#     digit = temp % 10
+#     sum = sum + digit ** order
+#     temp = temp // 10
+# if num == sum:
+#     print("The number is Armstrong")
+# else:
+#     print("The number is not Armstrong")
+
+
+
+
+
