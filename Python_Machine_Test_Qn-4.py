3d4a812beb0e8ea610149503e2576a04092df0bc
Anjana P R <pranjana333@gmail.com>
2025-11-06 19:48:44 +0530

    Refactor calculator input handling and operation selection

--- Python_Machine_Test_Qn-4.py
+++ Python_Machine_Test_Qn-4.py
@@ -7,7 +7,19 @@ class Calculator:
         return num1 * num2
     def division(self, num1, num2):
         return num1 / num2
-
+cal_obj = Calculator()
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operation = input("Enter the operation(addition, subtraction, multiplication, division): ")
+operation = input("Enter the operation(+, -, *, /): ")
+if operation == "+":
+    result = cal_obj.addition(num1, num2)
+    print("Result is: ", result)
+elif operation == "-":
+    result = cal_obj.subtraction(num1, num2)
+    print("Result is: ", result)
+elif operation == "*":
+    result = cal_obj.multiplication(num1, num2)
+    print("Result is: ", result)
+else:
+    result = cal_obj.division(num1, num2)
+    print("Result is: ", result)
