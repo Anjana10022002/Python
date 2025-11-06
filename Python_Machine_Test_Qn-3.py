f93d7a784b26dacb2c725c1ce54f39064cc3f217
Anjana P R <pranjana333@gmail.com>
2025-11-06 19:28:36 +0530

    Fixing syntax error

--- Python_Machine_Test_Qn-3.py
+++ Python_Machine_Test_Qn-3.py
@@ -2,7 +2,7 @@ array = [{"id":1, "name":"Rajesh"},
          {"id":2, "name":"Rahul"}, 
          {"id":3, "name":"Sruthi"}]
 
-num = int(input("Enter a ID number: "))
+num = int(input("Enter the ID number: "))
 
 for x in array:
     if num == x["id"]:
