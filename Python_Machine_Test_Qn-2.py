6531784304143ef7c49169304625a681c3d59f2d
Anjana P R <pranjana333@gmail.com>
2025-11-06 21:23:52 +0530

    Fixing syntax error.

--- Python_Machine_Test_Qn-2.py
+++ Python_Machine_Test_Qn-2.py
@@ -1,5 +1,6 @@
 num = int(input("Enter the number: "))
 print(f"The multiplication table of {num} is: ")
+
 for i in range(1, 11):
     multiplication = i * num
     print(f"{i} * {num} = {multiplication}")
