29d7ee6e5ec2b515d22be54625f16ecca551de2d
Anjana P R <pranjana333@gmail.com>
2025-11-07 14:59:53 +0530

    Created new calss employee inherited from person.

--- Python_Lab_Day8_Qn-1.py
+++ Python_Lab_Day8_Qn-1.py
@@ -2,3 +2,10 @@ class Person():
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    def show_details(self, name, age):
+        print(f"Name: {name}\n Age: {age}")
+class Employee(Person):
+    def __init__(self, name, age, employee_id, show_details):
+        super().__init__(name, age)
+        self.employee_id = employee_id
+        self.show_details = show_details
