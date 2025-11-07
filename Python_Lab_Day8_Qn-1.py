aad86463f2d9c2a59be223f079110737b6d0ad53
Anjana P R <pranjana333@gmail.com>
2025-11-07 15:09:14 +0530

    PartTime, adds an attribute project_name (string), and includes a show_details() method to print the consultant’s name, age, employee ID, working hours, and project name.

--- Python_Lab_Day8_Qn-1.py
+++ Python_Lab_Day8_Qn-1.py
@@ -18,3 +18,10 @@ class PartTime(Person):
         self.show_details = show_details
     def __str__(self):
         return super().__str__() + f"\nEmployee Name: {self.name}\nAge : {self.age}\nWorking Hours: {self.working_hours}"
+class Consultant(Employee, PartTime):
+    def __init__(self, name, age, employee_id, working_hours, project_name, show_details):
+        Employee.__init__(self, name, age, employee_id, show_details)
+        PartTime.__init__(self, name, age, working_hours, show_details)
+        self.project_name = project_name
+    def __str__(self):
+        return Employee.__str__(self) + f"\nName: {self.name}\nAge:{self.age}\nWorking Hours: {self.working_hours}"
