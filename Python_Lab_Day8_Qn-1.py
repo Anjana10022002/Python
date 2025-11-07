da56f09d17cee69aeef625dea1ab2d62e4f9a6ee
Anjana P R <pranjana333@gmail.com>
2025-11-07 15:19:22 +0530

    Fixing syntax error

--- Python_Lab_Day8_Qn-1.py
+++ Python_Lab_Day8_Qn-1.py
@@ -2,23 +2,20 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
     def show_details(self):
         print(f"Name: {self.name}, Age: {self.age}")
 
 class Employee(Person):
     def __init__(self, name, age, employee_id):
-        super().__init__(name, age)
+        Person.__init__(self, name, age)
         self.employee_id = employee_id
-
     def show_details(self):
         print(f"Name: {self.name}, Age: {self.age}, Employee ID: {self.employee_id}")
 
 class PartTime(Person):
     def __init__(self, name, age, working_hours):
-        super().__init__(name, age)
+        Person.__init__(self, name, age)
         self.working_hours = working_hours
-
     def show_details(self):
         print(f"Name: {self.name}, Age: {self.age}, Working Hours: {self.working_hours}")
 
@@ -27,7 +24,6 @@ class Consultant(Employee, PartTime):
         Employee.__init__(self, name, age, employee_id)
         PartTime.__init__(self, name, age, working_hours)
         self.project_name = project_name
-
     def show_details(self):
         print(f"Name: {self.name}, Age: {self.age}, Employee ID: {self.employee_id}, "
               f"Working Hours: {self.working_hours}, Project: {self.project_name}")
