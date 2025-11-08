c7aff2796d235ac149e4f11e407bdfb73399e75d
Anjana P R <pranjana333@gmail.com>
2025-11-08 22:35:20 +0530

    Fixing syntax error

--- Programiz-1_to_20.py
+++ Programiz-1_to_20.py
@@ -155,7 +155,7 @@ terms = int(input("Enter the number of terms: "))
 n1 = 0
 n2 = 1
 count = 0
-if terms <= 1:
+if terms <= 0:
     print("Enter a positive number")
 elif terms == 1:
     print(f"Fibonacci sequence of {terms} terms is: {n1}")
@@ -164,7 +164,7 @@ else:
     while count < terms:
         print(n1)
         seq = n1 + n2
-        ni = n2
+        n1 = n2
         n2 = seq
         count += 1
 
