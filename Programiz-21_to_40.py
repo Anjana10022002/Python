f6d48e4eda24548a9843fb5b181b8c9bf4ad56ab
Anjana P R <pranjana333@gmail.com>
2025-11-09 22:46:30 +0530

    Fixing syntax error

--- Programiz-21_to_40.py
+++ Programiz-21_to_40.py
@@ -17,8 +17,5 @@
 #     print(f"2 raised to {i} is {powers[i]}")
 
 # Qn-23 Python Program to Find Numbers Divisible by Another Number
-list_items = input("Enter the list of numbers: ")
-my_list = [int(x) for x in list_items.split()]
-for i in my_list:
-    result  = list(filter(lambda x:(x % i == 0), my_list))
-print(result)
+
+
